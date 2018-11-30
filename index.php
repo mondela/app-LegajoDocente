a1bda4bd992bd909bc0414c3f6ba983e1eb33ecb
mondela <44331021+mondela@users.noreply.github.com>
2018-11-30 15:51:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -73,6 +73,10 @@ $numReg = pg_num_rows($resultado);
 	<meta name="description" content="free-educational-responsive-web-template-webEdu">
 	<meta name="author" content="webThemez.com">
 	<title>Asistencia Docente</title>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	
 	<link rel="favicon" href="assets/images/favicon.png">
 	<link rel="stylesheet" media="screen" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
 	<!--link rel="stylesheet" media="screen" href="http://allfont.es/allfont.css?fonts=open-sans"-->
@@ -270,8 +274,8 @@ $numReg = pg_num_rows($resultado);
 	<script src="assets/js/custom.js"></script> -->
 	<!-- JavaScript lib for our digital clock -->
 	<script src="assets/js/clock.js"></script>
-	<script src="assets/js/bootstrap-transition.js"></script>
-    <script src="assets/js/bootstrap-modal.js"></script>
+	<!--script src="assets/js/bootstrap-transition.js"></script>
+    <script src="assets/js/bootstrap-modal.js"></script-->
     
 </body>
 </html>
